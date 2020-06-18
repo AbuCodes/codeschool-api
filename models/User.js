dcb43bfc9e07289887e1a79efcf9a5d6f2e3bc8a
AbuCodes <abuadel251@gmail.com>
2020-06-18 17:04:07 -0600

    added protected routes and authorization middleware

--- models/User.js
+++ models/User.js
@@ -36,27 +36,27 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// // Encrypt password using bcrypt
-// userSchema.pre('save', async function(next) {
-//   if (!this.isModified('password')) {
-//     next();
-//   }
+// Encrypt password using bcrypt
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
 
-//   const salt = await bcrypt.genSalt(10);
-//   this.password = await bcrypt.hash(this.password, salt);
-// });
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
-// // Sign JWT and return
-// userSchema.methods.getSignedJwtToken = function() {
-//   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-//     expiresIn: process.env.JWT_EXPIRE
-//   });
-// };
+// Sign JWT and return
+userSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
-// // Match user entered password to hashed password in database
-// userSchema.methods.matchPassword = async function(enteredPassword) {
-//   return await bcrypt.compare(enteredPassword, this.password);
-// };
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 // // Generate and hash password token
 // userSchema.methods.getResetPasswordToken = function() {
